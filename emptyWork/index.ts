f1984a41c0656439b82212a6b4647cdcea9c4d4c
XingTao <xingt@geovis.com.cn>
2023-07-21 13:59:26 +0800

    修改ion问题

--- emptyWork/index.ts
+++ emptyWork/index.ts
@@ -1,11 +1,10 @@
 import * as Cesium from "cesium";
-import { viewerConfig, initViewConfig } from "./src/config/earthConfig";
+import { viewerConfig, initViewConfig, cesiumToken } from "./src/config/earthConfig";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "./index.css";
 
 // token
-Cesium.Ion.defaultAccessToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmOTNiYjkwZi1iMzRlLTRjZWQtYWQxMy00MDVmMjk4YTc0YmMiLCJpZCI6MzY3MDksImlhdCI6MTY1NTE3OTc1N30.fv4nNIkCEEy3VqlaekWVcE1btEcge5_zCl_36AtusT0";
+Cesium.Ion.defaultAccessToken = cesiumToken;
 // 初始化地球
 let viewer = new Cesium.Viewer("MapContainer", viewerConfig);
 (viewer.cesiumWidget.creditContainer as HTMLElement).style.display = "none";
--- emptyWork/src/config/earthConfig.ts
+++ emptyWork/src/config/earthConfig.ts
@@ -1,5 +1,9 @@
 import * as Cesium from "cesium"
 
+// token
+const cesiumToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmOTNiYjkwZi1iMzRlLTRjZWQtYWQxMy00MDVmMjk4YTc0YmMiLCJpZCI6MzY3MDksImlhdCI6MTY1NTE3OTc1N30.fv4nNIkCEEy3VqlaekWVcE1btEcge5_zCl_36AtusT0";
+Cesium.Ion.defaultAccessToken = cesiumToken
+
 const viewerConfig = {
     terrainProvider: Cesium.createWorldTerrain({
         requestWaterMask: true,
@@ -41,4 +45,4 @@ const initViewConfig = {
     },
 }
 
-export { viewerConfig, initViewConfig }
+export { viewerConfig, initViewConfig, cesiumToken }
